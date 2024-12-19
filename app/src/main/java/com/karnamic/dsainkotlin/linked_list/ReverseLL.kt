9b61d79f8062ab7d9faff2ae0a242f292d72ed8f
vignesh <7vigneshshenoy@gmail.com>
2024-12-19 22:30:56 +0530

    Linked List practice

---
+++ app/src/main/java/com/karnamic/dsainkotlin/linked_list/ReverseLL.kt
@@ -0,0 +1,22 @@
+package com.karnamic.dsainkotlin.linked_list
+
+/***
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ * Example 1:
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ */
+fun <T> reverseList(head: SinglyLL.Node<T>?): SinglyLL.Node<T>? {
+    var curr = head
+    var prev: SinglyLL.Node<T>? = null
+
+    while (curr != null) {
+        val next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+
+    return prev
+}
