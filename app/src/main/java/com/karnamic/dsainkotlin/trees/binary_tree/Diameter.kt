04309889fb2e774043983a85ef5d7d1c9d90cd59
vignesh <7vigneshshenoy@gmail.com>
2024-12-28 16:05:00 +0530

    Tree Practice.

---
+++ app/src/main/java/com/karnamic/dsainkotlin/trees/binary_tree/Diameter.kt
@@ -0,0 +1,37 @@
+package com.karnamic.dsainkotlin.trees.binary_tree
+
+import com.karnamic.dsainkotlin.trees.TreeNode
+
+/**
+ * Given the root of a binary tree, return the length of the diameter of the tree.
+ * The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
+ * This path may or may not pass through the root.
+ *
+ * The length of a path between two nodes is represented by the number of edges between them.
+ *
+ *
+ * Example 1:
+ * Input: root = [1,2,3,4,5]
+ * Output: 3
+ * Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+ */
+
+fun diameterOfBinaryTree(root: TreeNode?): Int {
+    var maxDiameter = 0
+
+    fun dfs(root: TreeNode?): Int {
+
+        if (root == null) {
+            return 0
+        }
+
+        val leftHeight = dfs(root.left)
+        val rightHeight = dfs(root.right)
+        maxDiameter = maxOf(maxDiameter, leftHeight+rightHeight)
+        return 1 + maxOf(leftHeight, rightHeight)
+    }
+
+    dfs(root)
+
+    return maxDiameter
+}
