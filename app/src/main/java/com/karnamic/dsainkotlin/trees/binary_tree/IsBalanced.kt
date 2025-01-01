23ba74c3928852c30239b23672e47756c846c3ba
vignesh <7vigneshshenoy@gmail.com>
2025-01-01 20:33:49 +0530

    Tree practice.

---
+++ app/src/main/java/com/karnamic/dsainkotlin/trees/binary_tree/IsBalanced.kt
@@ -0,0 +1,28 @@
+package com.karnamic.dsainkotlin.trees.binary_tree
+
+import com.karnamic.dsainkotlin.trees.TreeNode
+
+/**
+ * Given a binary tree, determine if it is height-balanced
+ *
+ * height-balanced : A height-balanced binary tree is a binary tree
+ * in which the depth of the two subtrees of every node never differs by more than one.
+ */
+
+
+fun isBalanced(root: TreeNode?): Boolean {
+    fun dfs(root: TreeNode?): Pair<Boolean, Int> {
+        if (root == null) {
+            return Pair(true, 0)
+        }
+
+        val left = dfs(root.left)
+        val right = dfs(root.right)
+
+        val isBalanced = left.first && right.first && Math.abs(left.second - right.second) <= 1
+
+        return Pair(isBalanced, maxOf(left.second, right.second) + 1)
+    }
+
+    return dfs(root).first
+}
